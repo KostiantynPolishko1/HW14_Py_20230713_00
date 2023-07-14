b56c78abd85cfc8cf6ae562060329d3fcd6935d5
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-07-14 19:02:30 +0300

    6 Update logic of database

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import os
 from pathlib import Path
 import shutil
+import json
 
+def toJSON_File(_dict: dict, f_name: str) ->None:
+    with open(f_name, "w") as myFile:
+        json.dump(_dict, myFile)
+
+def JSON_File_toDict(f_name: str) ->dict:
+    _dict = {}
+    with open(f_name, "r") as myFile:
+        _dict = json.load(myFile)
+    return _dict
 
 def set_direct(dir: str) -> None:
     if os.path.exists(Path(Path.home(), dir)):
@@ -12,7 +22,7 @@ def set_direct(dir: str) -> None:
 
 
 def create_folders(dict_f: dict) -> None:
-    for i in range(len(dict_f)):
+    for i in range(1, len(dict_f)):
         if not os.path.isdir(dict_f[i][0]):
             os.mkdir(dict_f[i][0])
 
@@ -43,33 +53,41 @@ def move_file(arr_f: list) -> None:
 
 def move_folder(dict_f: dict) -> None:
     arr_f = []
-    for i in dict_folder:
-        arr_f.append(dict_folder[i][0])
+    for i in dict_f:
+        arr_f.append(dict_f[i][0])
 
     for f_name in os.listdir():
         if not f_name in arr_f:
-            shutil.move(f_name, Path(os.getcwd(), dict_f[5][0]))  # move inside folder 'Folders'
+            shutil.move(f_name, Path(os.getcwd(), dict_f[6][0]))  # move inside folder 'Folders'
 
 if __name__ == '__main__':
-    target_dir = 'Downloads\Telegram Desktop'
-    dict_folder = {0: ['Pictures', ['.bmp', '.png', '.jpg', '.jpeg', '.tif', '.tiff', 'gif']],
-                   1: ['Video', ['.mp4', '.mov', '.avi', '.avchd', '.flv', '.f4v', '.swf', '.mkv', '.webm', '.html5']],
-                   2: ['Music', ['.mp3', '.aac', '.alac', '.flac', '.wma', '.wav', '.aiff']],
-                   3: ['Documents', ['.doc', '.docx', '.pdf', '.txt', '.rtf', '.pptx', '.csv', '.xls', '.xlsx', '.xlsm']],
-                   4: ['Others'],
-                   5: ['Folders']
+
+    dict_folder = {0: ['Downloads\Telegram Desktop', 'data.txt'],
+                   1: ['Pictures', ['.bmp', '.png', '.jpg', '.jpeg', '.tif', '.tiff', 'gif']],
+                   2: ['Video', ['.mp4', '.mov', '.avi', '.avchd', '.flv', '.f4v', '.swf', '.mkv', '.webm', '.html5']],
+                   3: ['Music', ['.mp3', '.aac', '.alac', '.flac', '.wma', '.wav', '.aiff']],
+                   4: ['Documents', ['.doc', '.docx', '.pdf', '.txt', '.rtf', '.pptx', '.csv', '.xls', '.xlsx', '.xlsm']],
+                   5: ['Others'],
+                   6: ['Folders']
                    }
 
-    set_direct(target_dir)
+    # toJSON_File(dict_folder, dict_folder[0][1])
+    # dict_folder = JSON_File_toDict(data_file)
+    # print(list(dict_folder.keys()))
+
+
+    set_direct(dict_folder[0][0])
     create_folders(dict_folder)
 
-    move_picture(dict_folder[0])
-    move_video(dict_folder[1])
-    move_music(dict_folder[2])
-    move_doc(dict_folder[3])
-    move_file(dict_folder[4])
+    move_picture(dict_folder[1])
+    move_video(dict_folder[2])
+    move_music(dict_folder[3])
+    move_doc(dict_folder[4])
+    move_file(dict_folder[5])
     move_folder(dict_folder)
 
     print('DONE')
 else:
     print('ERROR!')
+
+#===================data back_up======================#
