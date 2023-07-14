3e63ed4350afde04a3590770f3ef346bdb27f62f
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-07-14 12:31:17 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import pathlib
+from pathlib import Path
+import shutil
+
+if __name__ == '__main__':
+    print(Path.home())
+    # os.chdir(r'C:\Users\User\Downloads')
+    # os.chdir(r'%HOMEPATH%')
+    # print(os.getcwd())
