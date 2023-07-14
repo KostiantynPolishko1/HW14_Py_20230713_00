8115f95da13f86b6f023646b163ee73594b2d9e6
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-07-14 17:32:51 +0300

    5 Done logic for next 4 functions 'move_video', 'move_music', 'move_doc', 'move_file', 'move_folder'

--- main.py
+++ main.py
@@ -21,6 +21,35 @@ def move_picture(arr_f: list) -> None:
         if Path(f_name).suffix.lower() in arr_f[1]:
             shutil.move(f_name, Path(os.getcwd(), arr_f[0]))  # move inside folder 'Pictures'
 
+def move_video(arr_f: list) -> None:
+    for f_name in os.listdir():
+        if Path(f_name).suffix.lower() in arr_f[1]:
+            shutil.move(f_name, Path(os.getcwd(), arr_f[0]))  # move inside folder 'Video'
+
+def move_music(arr_f: list) -> None:
+    for f_name in os.listdir():
+        if Path(f_name).suffix.lower() in arr_f[1]:
+            shutil.move(f_name, Path(os.getcwd(), arr_f[0]))  # move inside folder 'Music'
+
+def move_doc(arr_f: list) -> None:
+    for f_name in os.listdir():
+        if Path(f_name).suffix.lower() in arr_f[1]:
+            shutil.move(f_name, Path(os.getcwd(), arr_f[0]))  # move inside folder 'Documents'
+
+def move_file(arr_f: list) -> None:
+    for f_name in os.listdir():
+        if not Path(f_name).suffix == '':
+            shutil.move(f_name, Path(os.getcwd(), arr_f[0]))  # move inside folder 'Others'
+
+def move_folder(dict_f: dict) -> None:
+    arr_f = []
+    for i in dict_folder:
+        arr_f.append(dict_folder[i][0])
+
+    for f_name in os.listdir():
+        if not f_name in arr_f:
+            shutil.move(f_name, Path(os.getcwd(), dict_f[5][0]))  # move inside folder 'Folders'
+
 if __name__ == '__main__':
     target_dir = 'Downloads\Telegram Desktop'
     dict_folder = {0: ['Pictures', ['.bmp', '.png', '.jpg', '.jpeg', '.tif', '.tiff', 'gif']],
@@ -35,5 +64,12 @@ if __name__ == '__main__':
     create_folders(dict_folder)
 
     move_picture(dict_folder[0])
+    move_video(dict_folder[1])
+    move_music(dict_folder[2])
+    move_doc(dict_folder[3])
+    move_file(dict_folder[4])
+    move_folder(dict_folder)
 
-    print('END')
+    print('DONE')
+else:
+    print('ERROR!')
