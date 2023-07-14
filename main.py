274aa06e508948a3a24ed859549a83fc4bf186d2
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-07-14 13:48:44 +0300

    2 Done function 'create_folders' with logic to check if it already exist.

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import os
 from pathlib import Path
 import shutil
-def set_direct(folder: str) ->None:
-
-    if os.path.exists(Path(Path.home(), folder)):
-        os.chdir(Path(Path.home(), folder))
+def set_direct(dir: str) ->None:
+    if os.path.exists(Path(Path.home(), dir)):
+        os.chdir(Path(Path.home(), dir))
         print('\nset path', os.getcwd())
     else:
         print("\nERROR!\nTelegramDesktop is not available")
 
+def create_folders(arr: list) ->None:
+    for folder in arr:
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+
 if __name__ == '__main__':
-    app_folder = 'Downloads\Telegram Desktop'
-    set_direct(app_folder)
+    target_dir = 'Downloads\Telegram Desktop'
+    arr_folder = ['Pictures', 'Video', 'Music', 'Documents', 'Others']
+
+    set_direct(target_dir)
+    create_folders(arr_folder)
+
+    print('END')
 
